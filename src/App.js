9f39bb86fc106ba982ea3ea224a81b4063b876a5
Gio Garcia <giovannigarcia13128@gmail.com>
2023-10-24 12:25:52 -0700

    started div containers for app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='sidebar'>
+        <div className='upperSide'>
+          <div className='upperSideTop'><img src="" alt="" className="logo" /><span className="brand">GioGPT</span></div>
+          <button className='midBtn'><img src="" alt="" className="addBtn"/>New Chat</button>
+        </div>
+        <div className='lowerSide'>
 
+        </div>
+      </div>
+      <div className='main'>
+
+      </div>
     </div>
   );
 }
